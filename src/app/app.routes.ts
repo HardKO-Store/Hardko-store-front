6899857e7d91ec0622308d576399305e5a3988bd
NeoRise456 <neorise456@gmail.com>
2024-11-13 00:16:39 -0500

    feat(routes): add home route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeDashboardComponent} from './store/pages/home-dashboard/home-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeDashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: () => '/home',
+    pathMatch: 'full'
+  }
+];
