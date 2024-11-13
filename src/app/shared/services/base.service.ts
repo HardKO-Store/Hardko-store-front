e30f942a1b7c4a4c9d4c3705743bc65249eea340
NeoRise456 <neorise456@gmail.com>
2024-11-12 23:06:32 -0500

    chore: project setup.

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {throwError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
+  basePath: string = `${environment.serverBasePath}`;
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-type': 'application/json',
+    })
+  }
+
+  constructor(protected http: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    // Default error handling
+    if (error.error instanceof ErrorEvent) {
+      console.log(`An error occurred ${error.error.message}`);
+    } else {
+      // Unsuccessful Response Error Code returned from Backend
+      console.log(`Backend returned code ${error.status}, body was ${error.error}`);
+    }
+    return throwError(() => new Error('Something happened with request, please try again later'));
+  }
+
 }
